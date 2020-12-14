a25253ad8a2d43f12dd5289ece5a7d549b58621d
puglisac <alan.c.puglisi@gmail.com>
2020-12-14 13:09:25 -0600

    exercise complete

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //move gameObject to mouse position on left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 newPosition = Input.mousePosition;
+            newPosition.z = -Camera.main.transform.position.z;
+            newPosition = Camera.main.ScreenToWorldPoint(newPosition);
+            transform.position = newPosition;
+        }
+    }
+}
